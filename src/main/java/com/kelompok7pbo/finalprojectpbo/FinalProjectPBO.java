a49bc360ffffe8edeb390f653bd22164e25312dc
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-05-16 14:42:48 +0700

    Initial Commit On Essentials

---
+++ src/main/java/com/kelompok7pbo/finalprojectpbo/FinalProjectPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kelompok7pbo.finalprojectpbo;
+
+/**
+ *
+ * @author albia
+ */
+public class FinalProjectPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
