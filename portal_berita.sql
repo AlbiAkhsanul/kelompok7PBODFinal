fb6d478ddded0b0ef9f699704ae010e1753b6f5b
Alexander Stefanus <153767134+AlexStefanus@users.noreply.github.com>
2024-05-30 17:20:39 +0700

    Update portal_berita.sql

--- portal_berita.sql
+++ portal_berita.sql
@@ -1,6 +1,31 @@
-CREATE DATABASE IF NOT EXIST 'portal_berita'
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 30, 2024 at 08:44 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
-USE DATABASE 'portal_berita'
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portal_berita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
 
 CREATE TABLE `articles` (
   `ARTICLE_ID` int NOT NULL,
@@ -9,9 +34,22 @@ CREATE TABLE `articles` (
   `JUDUL_ARTICLE` longtext,
   `KONTEN_ARTICLE` text,
   `FOTO_ARTICLE` varchar(100) DEFAULT NULL,
-  `TANGGAL_ARTICLE` datetime NOT NULL,
-  `TANGGAL_DIBUAT` datetime NOT NULL
-)
+  `TANGGAL_ARTICLE` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`ARTICLE_ID`, `USER_ID`, `CATEGORY_ID`, `JUDUL_ARTICLE`, `KONTEN_ARTICLE`, `FOTO_ARTICLE`, `TANGGAL_ARTICLE`) VALUES
+(8, NULL, 1, 'b', 'b', NULL, '2024-05-30 14:19:27'),
+(9, NULL, 1, 'g', 'g', NULL, '2024-05-30 14:49:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookmarks`
+--
 
 CREATE TABLE `bookmarks` (
   `BOOKMARK_ID` int NOT NULL,
@@ -23,12 +61,34 @@ CREATE TABLE `bookmarks` (
   `KONTEN_BOOKMARK` text,
   `FOTO_BOOKMARK` varchar(100) DEFAULT NULL,
   `TANGGAL_ BOOKMARK` datetime NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
 
 CREATE TABLE `categories` (
   `CATEGORY_ID` int NOT NULL,
   `NAMA_CATEGORY` char(10) DEFAULT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`CATEGORY_ID`, `NAMA_CATEGORY`) VALUES
+(1, 'Sains'),
+(2, 'Humor'),
+(3, 'Horror'),
+(4, 'SciFi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `USER_ID` int NOT NULL,
@@ -38,47 +98,98 @@ CREATE TABLE `users` (
   `PASSWORD` varchar(100) DEFAULT NULL,
   `FOTO_USER` varchar(100) DEFAULT NULL,
   `IS_ADMIN` tinyint NOT NULL DEFAULT '0'
-) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
 
 INSERT INTO `users` (`USER_ID`, `NAMA`, `USERNAME`, `EMAIL`, `PASSWORD`, `FOTO_USER`, `IS_ADMIN`) VALUES
 (1, 'c', 'c', 'c', 'c', NULL, 0),
 (2, 'b', 'b', 'b', 'b', NULL, 0),
 (3, 'fadil', 'f', 'f', 'f', NULL, 0);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`ARTICLE_ID`),
   ADD KEY `FK_ARTICLE_MEMILIKI_CATEGORY` (`CATEGORY_ID`),
   ADD KEY `FK_ARTICLE_MEMILIKI_USER` (`USER_ID`);
 
+--
+-- Indexes for table `bookmarks`
+--
 ALTER TABLE `bookmarks`
   ADD PRIMARY KEY (`BOOKMARK_ID`),
   ADD KEY `FK_BOOKMARK_MEMILIKI_ARTICLE` (`ARTICLE_ID`),
   ADD KEY `FK_BOOKMARK_MEMILIKI_CATEGORY` (`CATEGORY_ID`),
   ADD KEY `FK_BOOKMARK_MEMILIKI_USER` (`USER_ID`);
 
+--
+-- Indexes for table `categories`
+--
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`CATEGORY_ID`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`USER_ID`);
 
-ALTER TABLE `articles`
-  MODIFY `ARTICLE_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `ARTICLE_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `bookmarks`
+--
 ALTER TABLE `bookmarks`
   MODIFY `BOOKMARK_ID` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `categories`
+--
 ALTER TABLE `categories`
-  MODIFY `CATEGORY_ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `CATEGORY_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `USER_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `articles`
+--
 ALTER TABLE `articles`
   ADD CONSTRAINT `FK_ARTICLE_MEMILIKI_CATEGORY` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `categories` (`CATEGORY_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `FK_ARTICLE_MEMILIKI_USER` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
+--
+-- Constraints for table `bookmarks`
+--
 ALTER TABLE `bookmarks`
   ADD CONSTRAINT `FK_BOOKMARK_MEMILIKI_ARTICLE` FOREIGN KEY (`ARTICLE_ID`) REFERENCES `articles` (`ARTICLE_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `FK_BOOKMARK_MEMILIKI_CATEGORY` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `categories` (`CATEGORY_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `FK_BOOKMARK_MEMILIKI_USER` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
