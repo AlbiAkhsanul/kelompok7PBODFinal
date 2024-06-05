3cea7e389eff1684dcba314bb54dd4cd54069698
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-06-05 21:43:30 +0700

    dah bisa nampilin data di home, sama benerin query

--- src/main/java/com/kelompok7pbo/finalprojectpbo/CreateArticle.java
+++ src/main/java/com/kelompok7pbo/finalprojectpbo/CreateArticle.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  *
  * @author albia
--- src/main/java/com/kelompok7pbo/finalprojectpbo/MyArticles.java
+++ src/main/java/com/kelompok7pbo/finalprojectpbo/MyArticles.java
@@ -120,7 +120,7 @@ public class MyArticles extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "No", "Title", "Category", "Action"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
